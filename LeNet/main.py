af2d078c06c6a096159d4ef9e8cc899d79060ecf
hypeopal <136549095+hypeopal@users.noreply.github.com>
2024-07-17 14:18:39 +0800

    add some notes

--- LeNet/main.py
+++ LeNet/main.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+# 数据预处理与加载
 batch_size = 128
 transform = tv.transforms.Compose(
     [tv.transforms.ToTensor(), tv.transforms.Normalize((0.1307,), (0.3081,))])
@@ -15,20 +16,21 @@ train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=Tru
 test_data_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 
+# 定义LeNet模型
 class MyNet(nn.Module):
     def __init__(self):
         super(MyNet, self).__init__()
-        self.conv_block1 = nn.Sequential(
+        self.conv_block1 = nn.Sequential(  # 第一层卷积
             nn.Conv2d(1, 16, 5),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
-        self.conv_block2 = nn.Sequential(
+        self.conv_block2 = nn.Sequential(  # 第二层卷积
             nn.Conv2d(16, 32, 5),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
-        self.dense_block = nn.Sequential(
+        self.dense_block = nn.Sequential(  # 全连接层
             nn.Linear(32 * 4 * 4, 128),
             nn.Linear(128, 10)
         )
@@ -43,13 +45,14 @@ class MyNet(nn.Module):
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = MyNet().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+criterion = nn.CrossEntropyLoss()  # 损失函数
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)  # 优化函数
 
 losses = []
 accuracies = []
 start_time = time.time()
 
+# 训练
 for i in tqdm(range(10)):
     for j, (batch_data, batch_label) in enumerate(train_data_loader):
         batch_data, batch_label = batch_data.to(device), batch_label.to(device)
@@ -61,12 +64,13 @@ for i in tqdm(range(10)):
         predicted = torch.max(prediction, 1)[1]
         correct = (predicted == batch_label).sum().item()
         accuracy = correct / batch_size * 100
-        losses.append(loss.item())
-        accuracies.append(accuracy)
+        losses.append(loss.item())  
+        accuracies.append(accuracy)  # 保存准确率
 
 end_time = time.time()
 print('训练花了： %d s' % int((end_time - start_time)))
 
+# 训练结果可视化
 fig, ax1 = plt.subplots()
 
 color = 'tab:red'
@@ -85,6 +89,7 @@ fig.tight_layout()
 plt.title('Training Loss and Accuracy')
 plt.show()
 
+# 测试
 correct = 0
 for batch_data, batch_label in tqdm(test_data_loader):
     batch_data, batch_label = batch_data.to(device), batch_label.to(device)
